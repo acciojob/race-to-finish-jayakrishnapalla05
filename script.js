38a6942fc9ac7a839bce9a38b6a4869151cdc086
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-23 14:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 window.promises = [];
 
 
-for(let i=0; i<5; i++)
-	{
-		const randomTime=Math.floor(Math.random()*5)+1;
-		const prom=new Promise((resolve, reject) =>{
-			setTimeOut(() =>{
-				resolve(`Promise is resolved in ${randomTime} seconds`);
-			}, randomTime*1000);
-		});
-		promises.push(prom);
-	}
-Promise.any(promises)
-       .then((item) =>{
-		   const Element=document.getElementById("output");
-		   Element.textContent=item;
-	   })
-       .catch((error) =>{
-		   console.error(error);
-	   });
+for (let i = 0; i < 5; i++) {
+  const delay = Math.floor(Math.random() * 5) + 1; // Generate random delay between 1 and 5 seconds
 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+
+  promises.push(promise);
+}
+Promise.any(promises)
+  .then((result) => {
+    const outputElement = document.getElementById("output");
+    outputElement.innerText = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // Do not change the code above this
 // add your promises to the array `promises`
